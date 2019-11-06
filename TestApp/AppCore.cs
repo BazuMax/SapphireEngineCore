0cd7b0213acf1ae3b2eccff18eaf8f528efb0861
BazuMax <bazuka5801@mail.ru>
2019-11-06 23:36:29 +0300

    feat: add test app

---
+++ TestApp/AppCore.cs
@@ -0,0 +1,19 @@
+﻿using SapphireEngine;
+
+namespace TestApp
+{
+    public class AppCore : SapphireType
+    {
+        public float Time = 0;
+        public override void OnUpdate()
+        {
+            Time += DeltaTime;
+            if (Time > 1)
+            {
+                Time -= 1;
+                
+                ConsoleSystem.Log("Kek");
+            }
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SapphireEngine;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Framework.FPSLimit = 120;
+            Framework.Initialization<AppCore>();
+        }
+    }
+}
